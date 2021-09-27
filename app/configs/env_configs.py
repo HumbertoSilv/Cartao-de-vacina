51524af1b9ca12e211fbb971e71e0c1f2b227f72
Humberto <matheushumberto520@gmail.com>
2021-09-27 14:48:13 -0400

    feat: configurando pasta configs/ database, env configs, migrate

---
+++ app/configs/env_configs.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+
+def init_app(app: Flask):
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'
+        ] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config[
+        'SQLALCHEMY_TRACK_MODIFICATIONS'
+        ] = bool(os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'))
+    app.config[
+        'JSON_SORT_KEYS'
+        ] = bool(os.environ.get('JSON_SORT_KEYS'))
